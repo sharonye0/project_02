c09d3164395605fddca915b88a719b7e8c5d571d
sharon <ali93456@gmail.com>
2022-08-10 13:56:11 +0300

    everything is formatted

--- script.js
+++ script.js
@@ -1,65 +1,50 @@
-const progressBar = document.querySelector('.progress');
+const progressBar = document.querySelector(".progress");
 
-const num = document.querySelectorAll('.num');
+const num = document.querySelectorAll(".num");
 const nums = Array.from(num);
 
-const prev = document.querySelector('.btn-prev');
-const next = document.querySelector('.btn-next');
-
-console.log(nums);
+const prev = document.querySelector(".btn-prev");
+const next = document.querySelector(".btn-next");
 
 const nextProgress = function (index, className) {
-  progressBar.classList.add(className)
-  setTimeout(function() {
-    nums[index].classList.add('active')
-  }, 550)
-}
+  progressBar.classList.add(className);
+  setTimeout(function () {
+    nums[index].classList.add("active");
+  }, 550);
+};
 
 const prevProgress = function (index, className) {
-  console.log(index, className)
-  progressBar.classList.remove(className)
-  nums[index].classList.remove('active')
-}
+  progressBar.classList.remove(className);
+  nums[index].classList.remove("active");
+};
 
 let currentActive = 1;
 
-next.addEventListener('click', function() {
+next.addEventListener("click", function () {
   currentActive++;
-  console.log(currentActive)
+  console.log(currentActive);
 
   if (currentActive === 2) {
-    nextProgress(1, "progress-two")
+    nextProgress(1, "progress-two");
     prev.disabled = false;
+  } else if (currentActive === 3) {
+    nextProgress(2, "progress-three");
+  } else if (currentActive === 4) {
+    nextProgress(3, "progress-four");
+    next.disabled = true;
   }
-
-  else if (currentActive === 3) {
-    nextProgress(2, "progress-three")
-  }
-
-else if (currentActive === 4) {
-  nextProgress(3, "progress-four")
-  next.disabled = true;
-}
-
 });
 
-
-
-prev.addEventListener('click', function() {
+prev.addEventListener("click", function () {
   if (!(currentActive <= 1)) currentActive--;
 
   if (currentActive === 3) {
-    prevProgress(3, "progress-four")
+    prevProgress(3, "progress-four");
     next.disabled = false;
-  }
-
-  else if (currentActive === 2) {
-    prevProgress(2, "progress-three")
-  }
-
-  else if (currentActive === 1) {
-    prevProgress(1, "progress-two")
+  } else if (currentActive === 2) {
+    prevProgress(2, "progress-three");
+  } else if (currentActive === 1) {
+    prevProgress(1, "progress-two");
     prev.disabled = true;
   }
-
-})
+});
